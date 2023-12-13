212d5c082c5c6ff914aea04f171d73f5b09b448a
Ensiform <ensiform@gmail.com>
2023-12-13 16:29:09 -0600

    Default to internet dedicated servers like 2.60b
    Fixes a regression from ET compared to q3e merge.

--- src/qcommon/common.c
+++ src/qcommon/common.c
@@ -4073,7 +4073,7 @@ void Com_Init( char *commandLine ) {
 
 	// get dedicated here for proper hunk megs initialization
 #ifdef DEDICATED
-	com_dedicated = Cvar_Get( "dedicated", "1", CVAR_INIT );
+	com_dedicated = Cvar_Get( "dedicated", "2", CVAR_INIT );
 	Cvar_CheckRange( com_dedicated, "1", "2", CV_INTEGER );
 #else
 	com_dedicated = Cvar_Get( "dedicated", "0", CVAR_LATCH );
